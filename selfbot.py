0916a1038609a753fd92ba136f973df029f5a729
1ups_ <61560124+1upses@users.noreply.github.com>
2022-03-24 23:09:00 +0100

    Update selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 import asyncio
 
-TOKEN = ""
+TOKEN = "enter yourt discord token here"
 client = commands.Bot(command_prefix=("."), self_bot=True)
+ponctuation = ['?','!',' ','.',',']
 
 @client.event
 async def on_ready():
@@ -20,9 +21,14 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    end_mess = message.content.endswith
+    text = message.content
+    text = text.lower()
+
+    if 'quoi' in text:
+        for i in ponctuation:
+            text = text.replace(i,'')
     
-    if end_mess('quoi') or end_mess('quoi ?') or end_mess('quoi?') or end_mess('Quoi'):
+    if text.endswith('quoi'):
         async with message.channel.typing():
             await asyncio.sleep(0.125)
         await message.channel.send('feur')
